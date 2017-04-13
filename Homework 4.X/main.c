6800ab0fc02f878720e7dbfa535c38d359c217b1
sourpatchkid9 <danielsours2018@u.northwestern.edu>
2017-04-12 21:28:36 -0500

    SPI

--- Homework 4.X/main.c
+++ Homework 4.X/main.c
@@ -1,14 +1,16 @@
 /* 
- * File:   HelloWorld.c
+ * File:   main.c
  * Author: Daniel
  *
- * Created on March 30, 2017, 11:29 AM
+ * Created on April 10, 2017, 9:10 PM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include<xc.h>           // processor SFR definitions
 #include<sys/attribs.h>  // __ISR macro
+#include <math.h>
+#define CS LATBbits.LATB7
 
 // DEVCFG0
 #pragma config DEBUG = OFF // no debugging
@@ -46,6 +48,41 @@
 #pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
 
 
+void initSPI1(){
+    TRISBbits.TRISB7 = 0;
+    RPB8Rbits.RPB8R = 0b0011;
+   // RPB7Rbits.RPB7R = 0b0011;
+
+    CS = 1;
+            
+  SPI1CON = 0;              // turn off the spi module and reset it
+  SPI1BUF;                  // clear the rx buffer by reading from it
+  SPI1BRG = 0x3;            // baud rate to 10 MHz [SPI4BRG = (80000000/(2*desired))-1]
+  SPI1STATbits.SPIROV = 0;  // clear the overflow bit
+  SPI1CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
+  SPI1CONbits.MSTEN = 1;    // master operation
+  SPI1CONbits.ON = 1;       // turn on spi 4
+}
+
+unsigned char spi_io(unsigned char o) {
+  SPI1BUF = o;
+  while(!SPI1STATbits.SPIRBF) { // wait to receive the byte
+    ;
+  }
+  return SPI1BUF;
+}
+
+void write_voltage(unsigned int channel, unsigned int voltage){
+    unsigned char bigdata = 0b01110000;
+    bigdata |= (channel << 7);
+    bigdata |= (voltage >> 4);
+    unsigned char littledata = 0b0;
+    littledata  |= ((voltage && 0b1111)<<4);
+    CS = 0;
+    spi_io(bigdata);
+    spi_io(littledata);
+    CS = 1;
+}
 int main() {
 
     __builtin_disable_interrupts();
@@ -66,16 +103,38 @@ int main() {
     TRISBbits.TRISB4 = 1;
     TRISAbits.TRISA4 = 0;
     LATAbits.LATA4 = 1;
-
+    
+ 
     __builtin_enable_interrupts();
 
+  
+    
+    initSPI1();
+    
+    unsigned int i = 0;
+    unsigned int sinwave[100];
+    unsigned int sawwave[100];
+    double wave;
+    
+    for (i = 0; i < 100; ++i){
+        wave = 255/2 + 255/2 * sin(2*3.14*i/100);
+        sinwave[i] = wave;
+        wave = (255/100)*i;
+        sawwave[i] = wave;
+    }
+      _CP0_SET_COUNT(0);
+        unsigned int count = 0;
     while(1) {
-        if (_CP0_GET_COUNT() >= 12000){
-            LATAINV = 0b100;
-            _CP0_SET_COUNT(0);
+        write_voltage(1,sinwave[count]);
+       write_voltage(0,sawwave[count]);
+        count++;
+        if (count >= 100){
+            count = 0;
         }
+        while (_CP0_GET_COUNT()<= 2400){
+            ;
+        }
+        _CP0_SET_COUNT(0);
     }
-	    // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
-		  // remember the core timer runs at half the sysclk 
-    }
+}
 
