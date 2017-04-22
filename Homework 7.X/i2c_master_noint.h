fd9f65f7dea860b426f7f57faee9ae4d3d604cfa
sourpatchkid9 <danielsours2018@u.northwestern.edu>
2017-04-22 16:42:03 -0500

    IMU start

---
+++ Homework 7.X/i2c_master_noint.h
@@ -0,0 +1,35 @@
+/* 
+ * File:   i2c_master_noint.h
+ * Author: Daniel
+ *
+ * Created on April 17, 2017, 9:23 PM
+ */
+
+#ifndef I2C_MASTER_NOINT_H
+#define	I2C_MASTER_NOINT_H
+
+void i2c_master_setup(void);
+
+void i2c_master_start(void);
+
+void i2c_master_restart(void);
+
+void i2c_master_send(unsigned char byte);
+
+unsigned char i2c_master_recv(void);
+
+void i2c_master_ack(int val);
+
+void i2c_master_stop(void);
+
+void initExpander();
+    
+void setExpander(char pin, char level);
+
+char getExpander();
+ 
+void I2C_read_multiple(unsigned char address, unsigned char register, unsigned char * data, int length);
+
+
+#endif	/* I2C_MASTER_NOINT_H */
+
